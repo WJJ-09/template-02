c84d1351340f37d0d4f3aa0e5464b06b36445fd4
wjjabc <wjjabc@Wikwen.com>
2025-01-04 14:51:22 +0800

    Revert "第四次修改"
    This reverts commit bc333f2637fced438964f4058fe948b5f263bdb0.

--- test.cpp
+++ test.cpp
@@ -3,18 +3,10 @@
 #include <string>
 
 using namespace std;
-int *test(){
-    int *p = new int(10);
-    return p;
-}
 
 int main()
 {
-    int *p = test();
-    cout << *p << endl;
-    cout << *p << endl;
-    vector<string>
-        msg{"你好", "C++"};
+    vector<string> msg {"你好","C++"};
     for (const string& word : msg)
     {
         cout << word <<" ";
